118918f08ccb73f45718d2822cd3daf805db9ba6
Nageshwar  Yadav <95100082+Ranveer098@users.noreply.github.com>
2023-08-17 07:45:11 +0530

    init

---
+++ tip_calaculator/index.js
@@ -0,0 +1,5 @@
+const billTotalInput=document.getElementById('billTotalInput')
+const tipInput=document.getElementById("tipInput")
+const numberofPeople=document,getElementById("numberofPeople")
+const perPersonTotal=document.getElementById("perPersonTotal")
+
