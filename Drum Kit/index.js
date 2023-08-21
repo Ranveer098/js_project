9500d1928560781b88e96a47dd007a9f7ae616a3
Nageshwar  Yadav <95100082+Ranveer098@users.noreply.github.com>
2023-08-21 10:02:58 +0530

    init

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,1 +1,9 @@
-a
+var numberOfButton=document.querySelectorAll(".drum").length;
+console.log(numberOfButton)
+for(var i=0; i<numberOfButton;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    alert("i got clicked");
+  
+});
+
+}
