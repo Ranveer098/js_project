656d2c4f9b94e21aa4da12a99af656e73464b6b2
Nageshwar  Yadav <95100082+Ranveer098@users.noreply.github.com>
2023-08-23 11:16:26 +0530

    final

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -4,11 +4,13 @@ for (var i = 0; i < numberOfButton; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 document.addEventListener("keypress", function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -48,4 +50,13 @@ function makeSound(key) {
     default:
       break;
   }
+}
+
+function buttonAnimation(currentKey){
+  var activeButton=document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+
 }
